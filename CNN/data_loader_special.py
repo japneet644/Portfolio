66479140b79251ea5532b5d55f8562a03e36318f
Japneet Singh <37984270+japneet644@users.noreply.github.com>
2024-09-11 18:34:56 -0400

    Update data_loader_special.py
    improved the code presentation

--- CNN/data_loader_special.py
+++ CNN/data_loader_special.py
@@ -1,19 +1,31 @@
 import pickle
 import gzip
-# from sklearn.model_selection import train_test_split
 import numpy as np
 
 def load_data_wrapper():
+    """
+    Load and preprocess lattice data from a pickle file.
+
+    Returns:
+        np.array: Reshaped training inputs
+    """
+    # Open the pickle file
     # f = open('./DATA/zeromean8by8lattice.pkl', 'rb')
     f = open('./DATA/8by8lattice.pkl', 'rb')
-    if (f.read(2) == '\x1f\x8b'):
+
+    # Check if the file is gzip-compressed
+    if f.read(2) == b'\x1f\x8b':
         f.seek(0)
-        return gzip.GzipFile(fileobj=f)
+        file_object = gzip.GzipFile(fileobj=f)
     else:
         f.seek(0)
-    training_inputs = pickle.load(f, encoding="latin1")
-    # print(np.array(training_inputs).shape) # 32,10000,8,8
-    #training_inputs = np.reshape(training_inputs,(320000, 8,8,1))
-    training_inputs = np.reshape(training_inputs,(320000, 8,8,1))
-    #training_inputs = np.reshape(training_inputs,(190000, 8,8,2))
+        file_object = f
+
+    # Load the data from the pickle file
+    training_inputs = pickle.load(file_object, encoding="latin1")
+
+    # Reshape the training inputs
+    # Original shape: 32, 10000, 8, 8
+    training_inputs = np.reshape(training_inputs, (320000, 8, 8, 1))
+
     return training_inputs
